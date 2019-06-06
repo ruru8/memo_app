90900ced1d4c9e69137e0de349aac17411f4fde4
ruru8 <chiharu.yasuda@classi.jp>
2019-06-06 10:25:12 +0900

    rubocopでリファクタ

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
-require 'sinatra'
-require 'sinatra/reloader'
+# frozen_string_literal: true
+
+require "sinatra"
+require "sinatra/reloader"
 
 # 配列確保
 @@call_title = {}
@@ -14,7 +16,7 @@ def read_process(id)
   @id = id
   @id_sym = @id.to_s.to_sym
   @title = @@call_title[@id_sym]
-  File.open("memo/#{@id.to_s}", "r") do |file|
+  File.open("memo/#{@id}", "r") do |file|
     @content = file.read
   end
 end
@@ -26,14 +28,14 @@ def write_process(id, title, content)
   @id_sym = @id.to_s.to_sym
   @@call_title[@id_sym] = @title
   # 編集
-  if File.exist?("memo/#{@id.to_s}")
-    File.open("memo/#{@id.to_s}","r+") do |file|
+  if File.exist?("memo/#{@id}")
+    File.open("memo/#{@id}", "r+") do |file|
       file.puts @content
     end
   # 新規作成
   else
     @@id_syms.push(@id_sym)
-    File.open("memo/#{@id.to_s}","w") do |file|
+    File.open("memo/#{@id}", "w") do |file|
       file.puts @content
     end
   end
@@ -44,43 +46,39 @@ def delete_process(id)
   @id_sym = @id.to_s.to_sym
   @@call_title.delete(@id_sym)
   @@id_syms.delete(@id_sym)
-  File.delete("memo/#{@id.to_s}")
+  File.delete("memo/#{@id}")
 end
 
-get '/' do
+get "/" do
   erb :top
 end
 
-get '/new' do
+get "/new" do
   erb :new
 end
 
-post '/new' do
+post "/new" do
   @content = @params[:memo]
   write_process(@content.object_id, @params[:name], @content)
   erb :top
 end
 
-get '/show/*' do |id|
+get "/show/*" do |id|
   read_process(id)
   erb :show
 end
 
-delete '/delete/*' do |id|
+delete "/delete/*" do |id|
   delete_process(id)
   erb :delete
 end
 
-get '/edit/*' do |id|
+get "/edit/*" do |id|
   read_process(id)
   erb :edit
 end
 
-patch '/edit-compleate/*' do |id|
+patch "/edit-compleate/*" do |id|
   write_process(id, @params[:name], @params[:memo])
   erb :edit_complete
 end
-
-
-
-
